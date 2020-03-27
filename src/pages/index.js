80d13e874069e653d419cdf58eabdbff2efdfc2b
Bea Malsky <beamalsky@datamade.us>
2020-03-27 14:50:42 -0500

    Add rechart

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ZoomLineChart from "../components/zoomlinechart"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <ZoomLineChart />
   </Layout>
 )
 
